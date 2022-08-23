c85e993603d27b6eeb5c3786a37a4323b8157cde
fipnooone <fipnone@gmail.com>
2022-08-23 12:21:20 +0700

    export default

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
  * @param name REACT_APP_{ENV_NAME}
  * @returns string or '' if not found
  */
-export const env = (name: string): string => {
+const env = (name: string): string => {
     return process.env?.[`REACT_APP_${name}`] ?? '';
 };
+
+export default env;
