493c8f16ea37b74dcffac9b2392a1b484ab3c624
fipnooone <fipnone@gmail.com>
2022-08-26 13:43:46 +0700

    tsc

---
+++ types/Components/Env.d.ts
@@ -0,0 +1,6 @@
+/**
+ *
+ * @param name REACT_APP_{ENV_NAME}
+ * @returns string or '' if not found
+ */
+export declare const env: (name: string) => string;
---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+import { env } from "./Components/Env";
+export default env;
